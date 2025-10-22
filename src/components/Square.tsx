2a765b9cf6b3c88bf5cdc172e9d7c6fa03bc2b3f
Daniel Schilling <schillingdaniel05@gmail.com>
2025-10-22 10:43:47 +0200

    Optimized formatting

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -16,8 +16,8 @@
  * Square of the connect-four game.
  * @param value gives the Square the information which color to display
  */
-function Square({ value }: { value: "red" | "blue" | null }) {
-  return <button className="square">{value}</button>;
+function Square({value}: {value: 'red' | 'blue' | null}) {
+    return <button className="square">{value}</button>;
 }
 
 export default Square;
