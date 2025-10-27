58fccbcb2afcbadf10f6dd715f13e6c48ddcd8b0
Daniel Schilling <schillingdaniel05@gmail.com>
2025-10-27 13:40:20 +0100

    Checks if value is null

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -17,12 +17,24 @@
  * @param value gives the Square the information which color to display
  * @param onClick gives Square a function when clicked
  */
-function Square({value, onClick}: {value: 'red' | 'blue' | null; onClick: () => void}) {
+function Square({
+    value,
+    onClick,
+}: {
+    value: 'red' | 'blue' | null;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+}) {
     return (
         <>
-            <button className="square" onClick={onClick}>{value}</button>
+            {!!value ? (
+                <button className="square" onClick={onClick}>
+                    {value}
+                </button>
+            ) : (
+                <button className="square">{value}</button>
+            )}
         </>
-    ); 
+    );
 }
 
 export default Square;
