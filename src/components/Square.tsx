7b681078bebb7603e40f7dbce0c24dfd248883d0
Daniel Schilling <schillingdaniel05@gmail.com>
2025-10-29 11:14:27 +0100

    Fixed build filure

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -15,11 +15,6 @@
 /**
  * Square of the connect-four game.
  * @param value gives the Square the information which color to display
-<<<<<<< HEAD
- */
-function Square({value}: {value: 'red' | 'blue' | null}) {
-    return <button className="square">{value}</button>;
-=======
  * @param onClick gives Square a function when clicked
  */
 function Square({
@@ -34,7 +29,6 @@ function Square({
             {value}
         </button>
     );
->>>>>>> 9-add-onclick-prop-to-square
 }
 
 export default Square;
