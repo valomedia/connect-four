f2b267abb22c3535bd2f907e6c55957af66bfb15
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2025-11-02 22:45:37 +0100

    Fix ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -36,13 +36,6 @@ export default defineConfig([
             reactDom.configs.recommended,
         ],
         languageOptions: {
-            parserOptions: {
-                project: [
-                    './tsconfig.node.json',
-                    './tsconfig.app.json'
-                ],
-                tsconfigRootDir: import.meta.dirname
-            },
             ecmaVersion: 2020,
             globals: globals.browser,
         },
